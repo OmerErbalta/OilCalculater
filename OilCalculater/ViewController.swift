b30d8c0e513a9355cc743bab6a904c4c2429b008
Omer Erbalta <erbalta80@gmail.com>
2024-05-03 22:45:34 +0300

    Initial Commit

---
+++ OilCalculater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OilCalculater
+//
+//  Created by OmerErbalta on 3.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
